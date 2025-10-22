095ee5cf70be5a4ad27b18f3260d31ae2635ac0f
Lu Junlin <lujl1923@mails.jlu.edu.cn>
2025-10-22 11:47:33 +0800

    make sim run successfully

---
+++ npc/csrc/tb_top.cpp
@@ -0,0 +1,47 @@
+#include "Vtop.h"
+#include "verilated.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#ifdef FST
+#include "verilated_fst_c.h"
+#else
+#include "verilated_vcd_c.h"
+#endif
+
+const int sim_time = 1000; // 或你想要的仿真步数
+
+int main(int argc, char **argv)
+{
+    VerilatedContext *contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vtop *top = new Vtop{contextp};
+    // wave
+    Verilated::traceEverOn(true);
+#ifdef FST
+    VerilatedFstC *tfp = new VerilatedFstC;
+    top->trace(tfp, 99);
+    tfp->open("build/obj_dir/simx.fst");
+#else
+    VerilatedVcdC *tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);
+    tfp->open("build/obj_dir/simx.vcd");
+#endif
+    while (contextp->time() < sim_time && !contextp->gotFinish())
+    {
+        contextp->timeInc(1);
+        int a = rand() & 1;
+        int b = rand() & 1;
+        top->a = a;
+        top->b = b;
+        top->eval();
+        tfp->dump(contextp->time());
+        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+        assert(top->f == (a ^ b));
+    }
+    tfp->close();
+    delete tfp;
+    delete top;
+    delete contextp;
+    return 0;
+}
