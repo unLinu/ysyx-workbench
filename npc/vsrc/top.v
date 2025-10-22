d5889bc95e16c00354098cd0871ee41393c8eea5
Lu Junlin <lujl1923@mails.jlu.edu.cn>
2025-10-22 19:46:16 +0800

    nvboard: Waterfall light example completed

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,7 +1,14 @@
 module top (
-        input	a,
-        input	b,
-        output	f
+        input   wire            clk,
+        input   wire            rst,
+        output  wire    [15:0]  led
     );
-    assign f = a ^ b;
+    // output declaration of module light
+
+    light u_light(
+              .clk 	(clk  ),
+              .rst 	(rst  ),
+              .led 	(led  )
+          );
+
 endmodule
