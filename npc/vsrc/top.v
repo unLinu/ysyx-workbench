87bd4fd6f2718e02911c6f5a93a00e618138691c
Lu Junlin <lujl1923@mails.jlu.edu.cn>
2025-10-29 12:17:44 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard 900991bf0fde6284bad629d2ff66777b6eaa41f9 Merge pull request #49 from SArthurX/fix-render-driver

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,20 +1,16 @@
 module top (
-        input   wire    [3:0]   A           ,
-        input   wire    [3:0]   B           ,
-        input   wire    [2:0]   sel         ,
-        output  wire    [3:0]   result      ,
-        output  reg     [7:0]   seg0        ,
-        output  wire    [7:0]   seg1        ,
-        output  wire            is_zero     ,
-        output  wire            overflow    ,
-        output  wire            carry_out       // 为 1 时产生进位或借位
+        input   wire            botton_clk      ,
+        input   wire            rst             ,
+        output  reg     [7:0]   Q               ,
+        output  wire    [7:0]   seg0            ,
+        output  wire    [7:0]   seg1            ,
+        output  wire    [7:0]   seg2
     );
 
-    assign  seg1[1] = ~result[3];
-    assign  {seg1[7:2], seg1[0]} = 7'b1111111;
+    wire    [11:0]  bcd;
 
     always @(*) begin
-        case (result)
+        case (bcd[3:0])
             4'b0000:
                 seg0 = 8'h03; // 0
             4'b0001:
@@ -32,35 +28,78 @@ module top (
             4'b0111:
                 seg0 = 8'h1f; // 7
             4'b1000:
-                seg0 = 8'h01; // -8
+                seg0 = 8'h01; // 8
             4'b1001:
-                seg0 = 8'h1f; // -7
-            4'b1010:
-                seg0 = 8'h41; // -6
-            4'b1011:
-                seg0 = 8'h49; // -5
-            4'b1100:
-                seg0 = 8'h99; // -4
-            4'b1101:
-                seg0 = 8'h0d; // -3
-            4'b1110:
-                seg0 = 8'h25; // -2
-            4'b1111:
-                seg0 = 8'h9f; // -1
+                seg0 = 8'h09; // 9
             default:
                 seg0 = 8'hff;
         endcase
     end
 
-    ALU u_ALU(
-            .A         	(A          ),
-            .B         	(B          ),
-            .sel       	(sel        ),
-            .result    	(result     ),
-            .is_zero   	(is_zero    ),
-            .overflow  	(overflow   ),
-            .carry_out 	(carry_out  )
-        );
+    always @(*) begin
+        case (bcd[7:4])
+            4'b0000:
+                seg1 = 8'h03; // 0
+            4'b0001:
+                seg1 = 8'h9f; // 1
+            4'b0010:
+                seg1 = 8'h25; // 2
+            4'b0011:
+                seg1 = 8'h0d; // 3
+            4'b0100:
+                seg1 = 8'h99; // 4
+            4'b0101:
+                seg1 = 8'h49; // 5
+            4'b0110:
+                seg1 = 8'h41; // 6
+            4'b0111:
+                seg1 = 8'h1f; // 7
+            4'b1000:
+                seg1 = 8'h01; // 8
+            4'b1001:
+                seg1 = 8'h09; // 9
+            default:
+                seg1 = 8'hff;
+        endcase
+    end
+
+    always @(*) begin
+        case (bcd[11:8])
+            4'b0000:
+                seg2 = 8'h03; // 0
+            4'b0001:
+                seg2 = 8'h9f; // 1
+            4'b0010:
+                seg2 = 8'h25; // 2
+            4'b0011:
+                seg2 = 8'h0d; // 3
+            4'b0100:
+                seg2 = 8'h99; // 4
+            4'b0101:
+                seg2 = 8'h49; // 5
+            4'b0110:
+                seg2 = 8'h41; // 6
+            4'b0111:
+                seg2 = 8'h1f; // 7
+            4'b1000:
+                seg2 = 8'h01; // 8
+            4'b1001:
+                seg2 = 8'h09; // 9
+            default:
+                seg2 = 8'hff;
+        endcase
+    end
+
+    LFSR u_LFSR(
+             .botton_clk 	(botton_clk  ),
+             .rst        	(rst         ),
+             .Q          	(Q           )
+         );
+
+    bin_to_bcd u_bin_to_bcd(
+                   .bin 	(Q    ),
+                   .bcd 	(bcd  )
+               );
 
 
 endmodule
