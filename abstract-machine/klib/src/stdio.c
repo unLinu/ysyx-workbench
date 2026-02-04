2c744e48a11d9a4edc84985bd8293b6627c9c15f
Lu Junlin <lujl1923@mails.jlu.edu.cn>
2026-02-04 17:01:40 +0800

    Implemented printf functionality and fixed some bugs.

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,24 +6,24 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  int ret = 0;
+  char out[1024];
+  va_list ap;
+  va_start(ap, fmt);
+  ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  putstr(out);
+  return ret;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
-int sprintf(char *out, const char *fmt, ...) {
   size_t len = 0;
-  va_list ap;
-  va_start(ap, fmt);
-
   while (*fmt != '\0') {
     if (*fmt == '%') {
       fmt++; // point to the character after '%'
       switch (*fmt++) {
       case 's': {
-        char *s = va_arg(ap, char*);
+        char *s = va_arg(ap, char *);
         size_t slen = strlen(s);
         strncpy(out + len, s, slen);
         len += slen;
@@ -31,7 +31,7 @@ int sprintf(char *out, const char *fmt, ...) {
       }
       case 'd': {
         int num = va_arg(ap, int);
-        char buf[10];
+        char buf[1024];
         size_t i, j;
         int num_len = 0;
 
@@ -39,18 +39,19 @@ int sprintf(char *out, const char *fmt, ...) {
           out[len++] = '0';
           break;
         }
+        unsigned int val = (unsigned int)num;
         if (num < 0) {
           out[len++] = '-';
-          num = -num;
+          val = -(unsigned int)(num);  // 防止溢出
         }
-        
+
         j = 0;
-        while (num != 0) { 
-          buf[j++] = num % 10;
-          num /= 10;
+        while (val != 0) {
+          buf[j++] = val % 10;
+          val /= 10;
           num_len++;
         }
-        
+
         for (i = 0; i < num_len; i++) {
           out[len++] = buf[--j] + '0';
         }
@@ -64,9 +65,18 @@ int sprintf(char *out, const char *fmt, ...) {
       out[len++] = *fmt++;
     }
   }
-  va_end(ap);
   out[len] = '\0';
+
   return len;
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  int ret = 0;
+  va_list ap;
+  va_start(ap, fmt);
+  ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
